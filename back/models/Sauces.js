59f13f9a2f6707a1cc133660e3550a73f54ac849
dalton-x <daltonx43@gmail.com>
2020-05-19 11:07:12 +0200

    ajout d'une valeur default pour les like/dislike
    cela pour evité le refresh de la page pour le premier dislike/like

--- back/models/Sauces.js
+++ back/models/Sauces.js
@@ -2,17 +2,17 @@ const mongoose = require('mongoose');
 
 // creation du model de sauvegarde dans mongoDB
 const saucesShema = mongoose.Schema({
-    userId: { type: String, require: true},             // UserId du createur
-    name: { type: String, require: true},               // Nom de la sauce
-    manufacturer: { type: String, require: true},       // Créateur de la sauce
-    description: { type: String, require: true},        // description de la sauce
-    mainPepper: { type: String, require: true},         // Ingredients qui pimente la sauce
-    imageUrl: { type: String, require: true},           // Adresse de l'image de presentation de la sauce
-    heat: { type: Number, require: true},               // Force de piquant de la sauce
-    likes: { type: Number},                             // nombre de Like recut
-    dislikes: { type: Number},                          // nombre de dislike recut
-    usersLiked: { type: Array},                         // Utilisateurs qui Like la sauce
-    usersDisliked: { type: Array},                      // Utilisateur qui DiLike la sauce
+    userId: { type: String, require: true},                       // UserId du createur
+    name: { type: String, require: true},                         // Nom de la sauce
+    manufacturer: { type: String, require: true},                 // Créateur de la sauce
+    description: { type: String, require: true},                  // description de la sauce
+    mainPepper: { type: String, require: true},                   // Ingredients qui pimente la sauce
+    imageUrl: { type: String, require: true},                     // Adresse de l'image de presentation de la sauce
+    heat: { type: Number, require: true},                         // Force de piquant de la sauce
+    likes: { type: Number, default:0, require: true},             // nombre de Like reçut
+    dislikes: { type: Number, default:0, require: true},          // nombre de dislike reçut
+    usersLiked: { type: Array, default:0, require: true},         // Utilisateurs qui Like la sauce
+    usersDisliked: { type: Array, default:0, require: true},      // Utilisateur qui DiLike la sauce
 });
 
 module.exports = mongoose.model('Sauces',saucesShema);
