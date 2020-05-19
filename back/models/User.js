e94ecdbff1c8345a5d80642f53f3d924cfb94848
dalton-x <daltonx43@gmail.com>
2020-05-19 11:55:50 +0200

    correction typo

--- back/models/User.js
+++ back/models/User.js
@@ -4,7 +4,7 @@ const uniqueValidator = require('mongoose-unique-validator');
 // creation du model de sauvegarde dans mongoDB
 const userSchema = mongoose.Schema({
   email: { type: String, required: true, unique: true },  // Email de l'utilisateur avec une gestion unique de l'email 
-  password: { type: String, required: true }              // ernegistrement du mot de pass
+  password: { type: String, required: true }              // enregistrement du mot de pass
 });
 
 userSchema.plugin(uniqueValidator);
