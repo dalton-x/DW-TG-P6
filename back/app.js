ce99552c5fbfe8968ca4d652d932320b112aa3f6
dalton-x <daltonx43@gmail.com>
2020-05-25 11:50:09 +0200

    controle header
    ajout d'une adresse specifique pour la connection a l'api

--- back/app.js
+++ back/app.js
@@ -8,10 +8,10 @@ const path = require('path');
 const saucesRoutes = require('./routes/sauces');
 const userRoutes = require('./routes/user');
 
-// mise en place de la function express()
+// mise en place de la fonction express()
 const app = express();
 
-// adresse de connextion pour la basse de donnée MongoDB
+// adresse de connexion pour la basse de donnée MongoDB
 mongoose.connect('mongodb+srv://Dalton_X:XocDMhsgIrI54Hdw@dw-tg-p6-3xktv.mongodb.net/Piquante_Sauce?retryWrites=true&w=majority', {
         useNewUrlParser: true,
         useUnifiedTopology: true
@@ -21,7 +21,7 @@ mongoose.connect('mongodb+srv://Dalton_X:XocDMhsgIrI54Hdw@dw-tg-p6-3xktv.mongodb
 
 // Creation des Headers pour les acces des utilisateur de l'API
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*'); //Controle des adresses qui peuvent se connecter a l'API * == All
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200'); //Controle des adresses qui peuvent se connecter a l'API // * == All
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization'); // reponse de la la pre-verification de la requete option
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS'); // Ajout des requete authorisé par le serveur pour le front-end
     next(); // passage au middelweare suivant
@@ -29,7 +29,7 @@ app.use((req, res, next) => {
 
 app.use(bodyParser.json()); //sert a parsé automatiquement le coprs de la reponse
 
-app.use('/sauce_image', express.static(path.join(__dirname, 'sauce_image')));   //ajout de l adresse d'un dossier static pour l'ajout de photo
+app.use('/sauce_image', express.static(path.join(__dirname, 'sauce_image')));   //ajout de l'adresse d'un dossier static pour l'ajout de photo
 
 // Déclaration des fichiers routes
 app.use('/api/sauces', saucesRoutes);
